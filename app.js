89196d1264ecd3aac63bd2a5c28f28c0a9715558
ilyas127 <57403728+ilyas127@users.noreply.github.com>
2020-06-12 22:01:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,244 @@
+//chapter 5
+//MATH EXPRESSIONS 
+   
+    // var item1 = 650;
+    // var item2 = 100;
+    // document.write("    price of item 1 is  " + "" + item1);
+    // document.write("<br>");
+    // document.write("price of item 2 "+ item2);
+    // document.write("<br>");
+    // document.write("quantity of item 1 is 3")
+    // document.write("<br>");
+    // document.write("quantity of item 1 is 3");
+    // document.write("<br>");
+    // document.write("shipping charges 100");
+    // document.write("<br>");
+    // var q1 = item1 *3;
+    // var q2 = item2 *7;
+    // var shipping = 100;
+    // var total;
+    
+    // total = q1+q2+shipping;
+    // document.write("total cost of your order is "+ total);
+
+
+
+
+    // var total = 980;
+    // var obtained = 804;
+    // var percentage;
+    // percentage = obtained/ total *100;
+    // document.write("total marks: "+ total)
+    // document.write("<br>");
+    // document.write("obtained marks: "+ obtained)
+    // document.write("<br>");
+    // document.write("percentage " + percentage);
+
+
+
+
+    // var pkr ;
+    // pkr = 104.80 *10;
+    // pkr = pkr + 28*25;
+    // document.write("CURRENCY IN PKR")
+    // document.write("<br>");
+
+    // document.write("<br>");
+
+    // document.write("total currency in pkr is "+pkr);
+
+
+
+
+
+
+    // var sapphire = 2;
+    // sapphire = sapphire+5;
+    // sapphire =sapphire*10;
+    // sapphire = sapphire/2
+    // document.write(sapphire);
+
+
+
+
+
+
+    // document.write("AGE CALCULATOR ")
+
+    // document.write("<br>");
+
+    // var current = 2020;
+    // var byear = 2001;
+    // var yourage;
+    // yourage = current-byear;
+    // document.write("current year "+ current);
+
+    // document.write("<br>");
+
+
+    // document.write("birth year "+ byear);
+
+
+    // document.write("<br>");
+
+
+    // document.write("your age is "+ yourage );
+
+
+
+
+
+    // document.write("The Lifetime Supply Calculator")
+    // document.write("<br>")
+    // var snack = sandwich;
+    // var age = 20;   
+    // var maxage = 65;
+    // var  estimated =3;
+
+
+    //chapter no 6 to 9
+
+
+
+
+
+
+
+    // var a = 10;
+    // document.write("Result");
+    // document.write("<br>");
+    // document.write("the value of a is "+a);
+    // document.write("<br>");
+    // document.write("<br>");
+    // document.write("..............................");
+    // document.write("<br>");
+
+    // a = ++a;
+
+    // document.write("the value of ++a is "+a);
+    // document.write("<br>");
+    // document.write("now the value of a is " +a);
+    // document.write("<br>");
+
+    // a = a++;
+
+    // document.write("now the value of a++ is "+a);
+    // document.write("<br>");
+    // document.write("now the value of a is " +a);
+    // a = --a;
+    // document.write("<br>");
+    // document.write("now the value of --a is " + a);
+    // document.write("<br>");
+    // document.write("now the value of a is " +a);
+    // a = a--;
+    // document.write("<br>");
+    // document.write("now the value of a-- is " + a);
+    // document.write("<br>");
+    // document.write("now the value of a is "+ a);
+
+
+
+
+
+
+
+
+
+    // var a =prompt("assalam o alaikum");
+    // alert(a)
+
+
+
+
+
+
+
+    // var a =prompt("enter number");
+    // document.write("<br>");
+
+
+    // document.write("Multiplication of " +a)
+    // var b;
+    // b = a*1;
+    // document.write(b)
+    // document.write("<br>");
+    // b = a*2;
+    // document.write(b)
+    // document.write("<br>");
+    // b = a*3;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*4;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*5;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*6;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*7;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*8;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*9;
+    // document.write(b)
+    // document.write("<br>");
+
+    // b = a*10;
+    // document.write(b)
+    // document.write("<br>");
+
+
+
+
+    // document.write("Subject   " + "Total Marks   " + "Obtained Marks    " +"percentage")
+    // document.write("<br>");
+    // var total = 100;
+    // var sub1 =prompt("enter subject 1");
+    // var sub2 =prompt("enter subject 2")
+    // var sub3 =prompt("enter subject 3")
+    // var obt1 =prompt("obtained marks for subject 1")
+    // var obt2 =prompt("obtained marks for subject 2")
+    // var obt3 =prompt("obtained marks for subject 3")
+ 
+    // var per1 = obt1/ total*100  
+    // var per2 = obt2/ total*100  
+    // var per3 = obt3/ total*100  
+
+
+    // document.write(sub1 + "" + total + "" + obt1 + "" + per1);
+    // document.write("<br>");
+    // document.write(sub2 + "" + total + "" + obt2 + "" + per2);
+    // document.write("<br>");
+    // document.write(sub3 + "" + total + "" + obt3 + "" + per3);
+
+
+
+
+
+
+//chapter 9 to 11 
+//par jani abhi ye complete nh h
+
+
+
+
+
+    // var city = prompt("enter city name");
+    // if(city == karachi){
+    // document.write("Welcome to city of lights");
+    // }
+
+    // else {
+    // document.write(" welcome to the city");
+    // }
+    // document.write(city);
